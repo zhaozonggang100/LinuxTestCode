c2456b83724076a6f7ba188b15f1cd5a4593ae5d
root <root@localhost.localdomain>
2020-05-22 16:13:59 +0800

    [add slab]

---
+++ module/slab/.slab.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/code/LinuxTestCode/module/slab/slab.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /root/code/LinuxTestCode/module/slab/slab.ko /root/code/LinuxTestCode/module/slab/slab.o /root/code/LinuxTestCode/module/slab/slab.mod.o ;  true
---
+++ module/slab/.tmp_versions/slab.mod
@@ -0,0 +1,3 @@
+/root/code/LinuxTestCode/module/slab/slab.ko
+/root/code/LinuxTestCode/module/slab/slab.o
+
