f278f3883357534192915d48796b726ae93b116d
Your Name <you@example.com>
2020-06-10 11:00:43 +0800

    20200610

---
+++ module/18_user_kernel_mem_malloc/user_kernel_mem_malloc.c
@@ -0,0 +1,89 @@
+#include <linux/kthread.h>
+#include <linux/time.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/nmi.h>
+#include <linux/init.h>
+#include <linux/uaccess.h>
+#include <linux/highmem.h>
+#include <linux/interrupt.h>
+#include <linux/capability.h>
+#include <linux/completion.h>
+#include <linux/kernel_stat.h>
+#include <linux/debug_locks.h>
+#include <linux/perf_event.h>
+#include <linux/security.h>
+#include <linux/notifier.h>
+#include <linux/profile.h>
+#include <linux/freezer.h>
+#include <linux/vmalloc.h>
+#include <linux/blkdev.h>
+#include <linux/delay.h>
+#include <linux/pid_namespace.h>
+#include <linux/smp.h>
+#include <linux/threads.h>
+#include <linux/timer.h>
+#include <linux/rcupdate.h>
+#include <linux/cpu.h>
+#include <linux/cpuset.h>
+#include <linux/percpu.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sysctl.h>
+#include <linux/syscalls.h>
+#include <linux/times.h>
+#include <linux/tsacct_kern.h>
+#include <linux/kprobes.h>
+#include <linux/delayacct.h>
+#include <linux/unistd.h>
+#include <linux/pagemap.h>
+#include <linux/hrtimer.h>
+#include <linux/tick.h>
+#include <linux/debugfs.h>
+#include <linux/ctype.h>
+#include <linux/ftrace.h>
+#include <linux/slab.h>
+#include <linux/init_task.h>
+#include <linux/binfmts.h>
+#include <linux/string.h>
+
+unsigned long pagemem;
+unsigned long kmallocmem;
+unsigned long vmallocmem;
+
+static int __init my_init(void)
+{
+	
+	printk("****************************\n");
+	pagemem = __get_free_pages(GFP_KERNEL ,3);
+    printk("<1>pagemem=%lx",pagemem);
+    kmallocmem = kmalloc(100,0);
+    printk("<1>kmallocmem=%lx",kmallocmem);
+    vmallocmem = vmalloc(1000000);
+    printk("<1>vmallocmem=%lx",vmallocmem);
+	printk("***************************\n");
+    return 0;
+}
+
+static void  __exit my_exit(void)
+{
+	if(pagemem)
+	{
+		free_pages(pagemem,3);
+	}
+	if(kmallocmem)
+	{
+	   // kfree(kmallocmem);	
+	}
+	if(vmallocmem)
+	{
+	   // vfree(vmallocmem);	
+	}
+
+    printk("module exit hello\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+MODULE_AUTHOR("Dragon");
+MODULE_LICENSE("GPL");
