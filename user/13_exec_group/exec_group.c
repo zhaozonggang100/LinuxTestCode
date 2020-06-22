12fbf96f838d5047412df75122fa09a636a101b2
Your Name <you@example.com>
2020-06-22 23:10:18 +0800

    202006

--- user/13_exec_group/exec_group.c
+++ user/13_exec_group/exec_group.c
@@ -1,18 +1,115 @@
+/* exec函数族的语法 */
+//https://blog.csdn.net/amoscykl/article/details/80354052
 #include<stdio.h>
 #include<unistd.h>
-int main()
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+ 
+ 
+int main(int argc, char *argv[])
 {
-        char *filename[]={"./bp",NULL};//BP是c文件编译链接后产生的可执行文件，目的是打印一条语句
-        char *envp[]={0,NULL};//如果依赖于新环境变量，这里才需要改
-        execve("./bp",filename,envp);//字符串是文件的路径需要具体到该运行文件名，直到文件夹貌似识别不了，或者命令的路径
-        /*注意，把下面的三个注释遇到去掉，后面的ls命令也无法执行，因为*execve执行后，相当于结束了当前的程序，切换到execve里面的那个程序*去了*/
-//      char *filename1[]={"ls","-al","/root",NULL};//命令和参数，记得要加NULL
-//      char *envp1[]={0,NULL};
-//      execve("/bin/ls",filename1,envp1);
-		while(1)
+	/* 字符串指针数组传递参数,使用包含v的exec函数参数 */
+	char *arg[] = {"ls","-a",NULL};
+	char *arg1[] = {"env",NULL};		//只用于execve函数
+ 
+	char *envp[] = {"NAME=amoscykl","EMAIL=xxxx@xx.com","PATH=/tmp",NULL};
+	char **ptr;				//指向环境表
+ 
+	// 打印出环境表
+	printf("自定义环境表\n");
+ 
+	for (ptr = envp; *ptr != 0; ptr++)
+		printf("%s \n",*ptr);
+ 
+	printf("\n");
+ 
+	sleep(2);
+ 
+	/* 子进程调用execl函数 */
+	if (fork() == 0)
+	{
+		//child1
+		printf("1-----execl-----\n");
+		if (execl("/bin/ls","ls","-a",NULL) == -1)
 		{
-			;
+			perror("execl error!");
+			exit(1);
 		}
-	
-        return 0;
+	}
+ 
+	sleep(2);
+ 
+	/* 子进程调用execv函数 */
+	if (fork() == 0)
+	{
+		//child2
+		printf("2-----execv-----\n");
+		if (execv("/bin/ls",arg) == -1)
+		{
+			perror("execv error!");
+			exit(1);
+		}
+	}
+ 
+	sleep(2);
+ 
+	/* 子进程调用execlp函数 */
+	if (fork() == 0)
+	{
+		//child3
+		printf("3-----execlp-----\n");
+		if (execlp("ls","ls","-a",NULL) == -1)
+		{
+			perror("execlp error!");
+			exit(1);
+		}
+	}
+ 
+	sleep(2);
+ 
+	/* 子进程调用execvp函数 */
+	if (fork() == 0)
+	{
+		//child4
+		printf("4-----execvp-----\n");
+		if (execvp("ls",arg) == -1)
+		{
+			perror("execvp error!");
+			exit(1);
+		}
+	}
+ 
+	sleep(2);
+ 
+	/* 子进程调用execle函数 */
+	if (fork() == 0)
+	{
+		//child5
+		printf("5-----execle-----\n");
+		if (execle("/usr/bin/env","env",NULL,envp) == -1)		//使用自定义的环境表,并打印出自定义环境变量
+		{
+			perror("execle error!");
+			exit(1);
+		}
+	}
+ 
+	sleep(2);
+ 
+	/* 子进程调用execve函数 */
+	if (fork() == 0)
+	{
+		//child6
+		printf("6-----execve-----\n");
+		if (execve("/usr/bin/env",arg1,envp) == -1)				//使用自定义的环境表，并打印出自定义环境变量
+		{
+			perror("execve error!");
+			exit(1);
+		}
+	}
+ 
+	sleep(2);
+ 
+	printf("over!\n");
+	return 0;
 }
