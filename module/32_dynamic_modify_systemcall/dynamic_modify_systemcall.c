85959e2b24255e1c41d449a6effe293a292f63f6
Your Name <you@example.com>
2020-06-26 11:28:49 +0800

    202006

--- module/32_dynamic_modify_systemcall/dynamic_modify_systemcall.c
+++ module/32_dynamic_modify_systemcall/dynamic_modify_systemcall.c
@@ -1,6 +1,3 @@
-/*
-https://blog.csdn.net/CrazyHeroZK/article/details/87359529
-*/
 #include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -11,82 +8,36 @@ https://blog.csdn.net/CrazyHeroZK/article/details/87359529
 #include <linux/mm.h>
 #include <asm/io.h>
 
-static unsigned long syscall_tbl_addr;
-static unsigned long saved_syscall_addr;
-static int syscall_nr = 223;
+#define D_NR_mycall 40
+#define sys_call_table_adress 0xffffffffab000200
 
-// 系统调用实现
-int syscall_impl(const char __user *msg, size_t len)
+unsigned long * sys_call_table = 0;
+static int (*anything_saved)(void);
+
+long sys_mycall(void)
 {
-    char *k_msg = NULL;
-
-    if (!msg || !len)
-        return -EINVAL;
-    
-    k_msg = kmalloc(len, GFP_KERNEL);
-    if (!k_msg)
-        return -ENOMEM;
-    
-    if (copy_from_user(k_msg, msg, len)) {
-        kfree(k_msg);
-        return -EFAULT;  
-    }
-    printk(KERN_INFO "do syscall in kernel mode[%d] : %.*s\n", len, k_msg);
-    kfree(k_msg);
-    // do something
-    return 0;
+	printk("dragon[%d][%s]\n",current->pid,current->comm);
+	return current->pid;
 }
 
-// 修改系统调用表，任意选择上述介绍的一种
-
-unsigned long syscall_table_modify(unsigned long *tbl, unsigned nr, unsigned long new_addr)
-{
-	unsigned long   old_addr;
-	unsigned char   *p;
-
-#ifndef X86_CR0_WP
-  #define X86_CR0_WP    (1UL << 16)
-#endif
-	preempt_disable();
-	write_cr0(read_cr0() & ~X86_CR0_WP);//去掉写保护
-	p = (unsigned char *)&tbl[nr];
-	old_addr = ACCESS_ONCE(*(unsigned long *)p); //保存修改系统调用号的槽位中的函数地址
-	ACCESS_ONCE(*((unsigned long *)p)) = new_addr;
-	smp_mb();
-	write_cr0(read_cr0() | X86_CR0_WP);//增加写保护
-	preempt_enable();
-
-	return old_addr;
-
-}
 
 static int __init init_mod(void)
 {
-    printk(KERN_INFO "Kernel Module install!\n");
-    if (!syscall_tbl_addr) {
-        printk(KERN_ERR "Parameter was invalid\n");
-        return -EINVAL;
-    }
-    saved_syscall_addr = syscall_table_modify((unsigned long *)syscall_tbl_addr, 
-            syscall_nr, (unsigned long)syscall_impl);
-    return 0;
+	printk("*******start******\n");
+	sys_call_table = (unsigned long *)sys_call_table_adress;
+	anything_saved = (int(*)(void))sys_call_table[D_NR_mycall];
+	sys_call_table[D_NR_mycall] = (unsigned long)&sys_mycall;
+	return 0;
 }
 
 static void __exit exit_mod(void)
 {
-    printk(KERN_INFO "Kernel Module uninstall!\n");
-    saved_syscall_addr = syscall_table_modify((unsigned long *)syscall_tbl_addr, 
-            syscall_nr, (unsigned long)saved_syscall_addr);
-    WARN_ON(saved_syscall_addr != (unsigned long)syscall_impl);
+	sys_call_table[D_NR_mycall] = (unsigned long )anything_saved;
+	printk("*******exit******\n");
+	
 }
 
 module_init(init_mod);
 module_exit(exit_mod);
-
-//将变量设置为文件系统关联，这样才能传递参数
-module_param(syscall_tbl_addr, ulong, 0640);
-MODULE_PARM_DESC(syscall_tbl_addr,"system call table address, need to be provided when the module is installed");
-
 MODULE_AUTHOR("Dragon");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("System call modify");
