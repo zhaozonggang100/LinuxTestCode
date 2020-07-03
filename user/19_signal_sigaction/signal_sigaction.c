ea785130baed3614dbc2e73ff4ff47812f1f8051
Your Name <you@example.com>
2020-07-03 18:11:20 +0800

    202007

--- user/19_signal_sigaction/signal_sigaction.c
+++ user/19_signal_sigaction/signal_sigaction.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <signal.h>
-
+#include <time.h>
 void handle(int signo)
 {
 	printf("zhaozongagng_signal signal [%d] \n",signo);
@@ -16,17 +16,48 @@ int main()
 {
 
 //sighandler_t signal(int signum, sighandler_t handler);
-	signal(2 , handle);
-	
-	
+printf("pid = %d \n",getpid());
+printf("ok1 [%p]\n",handle);
+printf("ok1 [%p]\n",handler);
 
+sleep(1);
+
+	signal(2 , handle);
+	sleep(1);
+	
+	signal(3 , handle);
+	sleep(1);
+	
+	signal(4 , handler);
+	sleep(1);
+	
+	signal(6 , handle);
+	sleep(1);
+	
+	signal(9 , handle);
+	sleep(1);
+
+
+	signal(32 , handle);
+	sleep(1);	
+	
+	signal(33 , handler);
+	sleep(1);
+	
+	signal(40, handle);
+	sleep(1);
+	
+printf("ok [%p]\n",handle);
 //int sigaction(int signum, const struct sigaction *act,struct sigaction *oldact);
+	
+	/*
 	struct sigaction act ,oact;
 	act.sa_handler = handler ;
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = 0;
 	
 	sigaction(2,&act ,&oact);//给2信号分配一个新的处理函数
+	*/
 	
 	while(1);
 	return 0;
--- user/20_kill/kill.c
+++ user/20_kill/kill.c
@@ -12,14 +12,16 @@ printf("input pid you want to send SIGINT:");
 scanf("%d",&pid);
 while(1)
 {
+	/*
 	for(i=1 ; i<64 ;i++)
 	{
 		kill(pid,i);
 
 	}
-	sleep(1);
-	printf("*****[%d]\n",i);
-	i=1;
+	*/
+	kill(pid,40);
+	//sleep(1);
+	//printf("*****[%d]\n",i++);
 }
 
 		
