be84d8e5f7bd62c925eb66461a67da72a450a4e9
root <root@192.168.145.178>
2020-08-16 16:42:10 +0800

    202008

--- module/52_per_cpu_function/per_cpu_function.c
+++ module/52_per_cpu_function/per_cpu_function.c
@@ -1,4 +1,5 @@
 //参考实验https://www.jianshu.com/p/f67517b3a150
+//per-cpu是每个CPU私有的数据
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -13,7 +14,23 @@
 per_cpu 
 DEFINE_PER_CPU
 */
+/*
+per-cpu 静态和动态分配
+1 静态分配
+        DEFINE_PER_CPU(long,cpuvar) = 10;
+		DECLARE_PER_CPU(long,cpuvar) = 10;
+2 动态分配
+        cpualloc = alloc_percpu(long);
+	释放函数：
+		free_percpu(cpualloc);
+*/
 
+
+/*对于动态定义的per-cpu变量使用如下函数访问：
+						get_cpu_var
+						put_cpu_
+
+*/
 static DEFINE_PER_CPU(long,cpuvar) = 10;
 
 static long __percpu *cpualloc;
@@ -22,18 +39,30 @@ static int __init my_oops_init(void)
 {
 	int cpu ;
 	printk("dragon init \n");
-	for_each_possible_cpu(cpu){
+	/*
+	for_each_online_cpu()//现在在线的CPU个数 循环遍历
+	for_each_possible_cpu()//可以支持cpu的个数
+	for_each_present_cpu()
+	*/
+	for_each_online_cpu(cpu){
+		//获取数据 并赋值
 		per_cpu(cpuvar , cpu) = 15;
+		/*对于静态定义的per-cpu变量 可以使用：
+										get_cpu_var()//访问变量
+										put_cpu_var()//修改变量
+		*/
 		printk("01:[%d] [%ld] \n",cpu ,get_cpu_var(cpuvar));
+		//和get_cpu_var配套使用 恢复抢占
 		put_cpu_var(cpuvar);
 	}
-	
+	//	修改其中一个perCPU 数据为20
 	__this_cpu_write(cpuvar, 20);
 	
 	cpualloc = alloc_percpu(long);
 	
 	//为这个变量设置所有CPU变量
-	for_each_possible_cpu(cpu){
+	for_each_online_cpu(cpu){
+		//获取数据并赋值
 		*per_cpu_ptr(cpualloc, cpu)= 100;
 		printk("02:[%d] [%ld] \n",cpu ,*per_cpu_ptr(cpualloc, cpu));
 	}
@@ -46,8 +75,10 @@ static void __exit my_oops_exit(void)
 	int  cpu;
 	printk("goodbye\n");
 	
-	for_each_possible_cpu(cpu){
+	for_each_online_cpu(cpu){
+		//per_cpu(cpuvar, cpu)仅仅 获取数据
 		printk("03:[%d] [%ld] \n",cpu ,per_cpu(cpuvar, cpu));
+		
 		printk("03:[%d] [%ld] \n",cpu,*per_cpu_ptr(cpualloc, cpu));
 	}
 	free_percpu(cpualloc);
